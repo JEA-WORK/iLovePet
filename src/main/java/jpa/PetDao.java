0c6bd5f9736cc266cbdb81f20e762692994f6343
josuequeiroz <josuequeiroz.inf.ufg.br>
2017-11-29 15:40:49 -0200

    Em andamento

--- src/main/java/jpa/PetDao.java
+++ src/main/java/jpa/PetDao.java
@@ -35,11 +35,67 @@ public class PetDao {
 		em.close();
 	}
 
-	public static void alterar(String cod, String nome) {
+	public static void alterar(String cod, String nome) {}
+	/*	 
+		try {
+			EntityManager em = emf.createEntityManager();
+ 
+			PreparedStatement em = prepareStatement("Update pet SET nome = ?, dono = ?, telefone = ?, "
+					+ "apelido = ?, raca = ?, descricao = ?"
+						+ " WHERE cod = ? ");
+			em.setString(1, pet.getNome());
+			em.setString(2, pet.getTelefone());
+			em.setLong(5, pet.getCod());
+			em.execute();
+			em.close();
+			conexao.close();
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+		 	
+*/
+	
 
-	public static void excluir(String cod) {
+	public static void excluir(String cod) {}
+		/*	
+		try {
+			EntityManager em = emf.createEntityManager();
+			/*Fazer uma consulta para selecionar o cod a ser excluido
+					.prepareStatement("Delete from	tbaluno where matricula = ? ");
+					
+			em.setFlushMode(null);
+			em.persist(pet);
+			em.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+*/
+	/*
+	 * 	public Aluno consultar(Aluno aluno) {
+		try {
+			Connection conexao = getConexao();
+			PreparedStatement pstm = conexao
+					.prepareStatement("Select * from tbaluno where matricula =	?");
+			pstm.setLong(1, aluno.getMatricula());
+			ResultSet rs = pstm.executeQuery();
+			if (rs.next()) {
+				aluno.setMatricula(rs.getLong("matricula"));
+				aluno.setNome(rs.getString("nome"));
+				aluno.setTelefone(rs.getString("telefone"));
+				aluno.setEmail(rs.getString("email"));
+				aluno.setDataCadastro(new java.util.Date(rs.getDate("datacadastro").getTime()));
+			}
+			pstm.close();
+			conexao.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return aluno;
+	}
+	 * */
 
 	public static List<Pet> listar() {
 		EntityManager em = emf.createEntityManager();
